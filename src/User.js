cb9c7ac0e0d88eef977a030fa8cdd156f957f078
Mateus <mateusmpi@gmail.com>
2024-06-05 17:03:52 -0300

    fix: username editing

--- src/User.js
+++ src/User.js
@@ -1,14 +1,24 @@
+import React from "react";
 
 export default function User() {
+
+    const [nameUser, setNameUser] = React.useState("catanacomics");
+
+    function pencilButton() {
+        const newNameUser = prompt("Digite seu nome de usuário!");
+        (newNameUser === "" || newNameUser === null) ? setNameUser(nameUser) : setNameUser(newNameUser);
+    }
+
     return (
         <div className="usuario">
             <img src="assets/img/catanacomics.svg" alt="imagem de perfil" />
             <div className="texto">
                 <span>
-                    <strong>catanacomics</strong>
-                    <ion-icon name="pencil"></ion-icon>
+                    <strong>{nameUser}</strong>
+                    <ion-icon name="pencil" onClick={pencilButton}></ion-icon>
                 </span>
             </div>
         </div>
     )
 }
+
